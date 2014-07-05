467178e5535b9f0e8c63f99358be6cb4376d9950
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2014-07-05 23:33:53 +0200

    Fix autoloader. Require class only if the file exists, otherwise let other autoloader functions try to load classes.

--- src/Slova/Core/Autoloader.php
+++ src/Slova/Core/Autoloader.php
@@ -3,8 +3,19 @@
 namespace Slova\Core;
 
 class Autoloader {
+
+    protected $includePath;
+
+    public function __construct() {
+        $this->includePath = explode(PATH_SEPARATOR, get_include_path());
+    }
+
     public function doIt($className) {
         $fileName = str_replace('\\', '/', $className) . '.php';
-        require_once $fileName;
+        foreach ($this->includePath as $path) {
+            if (file_exists($path . '/' . $fileName)) {
+                require_once $path . '/' . $fileName;
+            }
+        }
     }
 }
