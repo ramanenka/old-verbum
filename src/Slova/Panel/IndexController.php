b39199e979b5813c0185a4ec946123db98de9cad
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2014-11-12 21:14:08 +0100

    Add scratch of Panel module

---
+++ src/Slova/Panel/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Slova\Panel;
+
+use Slova\Core\Response;
+
+class IndexController {
+
+    public function indexAction(Response $r) {
+        $r->setHeader('Content-Type', 'text/html; charset=utf-8');
+        $r->setContent('ololo');
+    }
+}
