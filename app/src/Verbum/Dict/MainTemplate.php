1d4a3f21a378ece4b73082e03988e1f097f8d7e2
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2015-06-28 01:24:40 +0200

    Covers #26: Implement statics boosting

--- app/src/Verbum/Dict/MainTemplate.php
+++ app/src/Verbum/Dict/MainTemplate.php
@@ -30,14 +30,47 @@ class MainTemplate
         $this->template = $template;
     }
 
+    /**
+     * Returns array of javascript files to load on the main page
+     *
+     * @return array
+     */
     public function getJSFilesList()
     {
-        return $this->jsFiles;
+        return array_map([$this, 'addVersionPostfix'], $this->jsFiles);
     }
 
+    /**
+     * Returns array of css files to load on the main page
+     *
+     * @return array
+     */
     public function getCSSFilesList()
     {
-        return $this->cssFiles;
+        return array_map([$this, 'addVersionPostfix'], $this->cssFiles);
+    }
+
+    /**
+     * Adds version to the specified file to be able to boost all statics
+     * when it has changed
+     *
+     * @param $file
+     * @return string
+     */
+    protected function addVersionPostfix($file)
+    {
+        return $file . '?v=' . $this->fileMTime('public/' . $file);
+    }
+
+    /**
+     * Returns file's modified time if file exists
+     *
+     * @param $file
+     * @return bool|int
+     */
+    protected function fileMTime($file)
+    {
+        return file_exists($file) ? filemtime($file) : false;
     }
 
     public function render()
