38eb754d6ec47d467871ff3d8b6e078bad1c9187
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2015-06-29 22:07:22 +0200

    Fix checkout command

--- bin/post-receive.bash
+++ bin/post-receive.bash
@@ -4,4 +4,4 @@ repo=/var/repo/verbum.git
 cd $repo
 #composer install --no-dev
 
-git --work-tree=/var/www/html/slounik --git-dir=$repo checkout -f
+git --work-tree=/var/www/html/slounik --git-dir=$repo checkout -f production
