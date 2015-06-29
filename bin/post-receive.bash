38a4ed8bdfad419af72be2e299be4e1b05a59655
Vadzim Ramanenka <vramanenka@sugarcrm.com>
2015-06-29 22:14:02 +0200

    Install composer packages on deploy

--- bin/post-receive.bash
+++ bin/post-receive.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-repo=/var/repo/verbum.git
-cd $repo
-#composer install --no-dev
+composer install --no-dev
 
 git --work-tree=/var/www/html/slounik --git-dir=$repo checkout -f production
